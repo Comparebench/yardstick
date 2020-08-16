ece7beca9a827a2878a95473be7dee6d3c99e96e
Jeremy <jdavis.ini@gmail.com>
2020-08-16 12:37:36 -0500

    Add CBrunner, add batch file creation, add folder select dialog. Show CPU model name and resulting CB score

--- yardstick/MainWindow.xaml.cs
+++ yardstick/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 using LibreHardwareMonitor.Hardware;
 
 namespace yardstick{
@@ -22,7 +25,6 @@ namespace yardstick{
     
     public partial class MainWindow : Window, INotifyPropertyChanged{
         private string _modelName;
-
         public string ModelName
         {
             get { return _modelName; }
@@ -30,6 +32,16 @@ namespace yardstick{
             {
                 _modelName = value;
                 // OnPropertyChanged("ModelName");
+            }
+        }
+        private string _cbScore;
+        public string cbScore
+        {
+            get { return _cbScore; }
+            set
+            {
+                _cbScore = value;
+                OnPropertyChanged("cbScore");
             }
         }
 
@@ -57,10 +69,51 @@ namespace yardstick{
                 }
                
             }
-            // new BuildBat().Build();
+            
         }
         //INotifyPropertyChanged members
         public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private void SelectCinebenchLocation(object sender, RoutedEventArgs e)
+        {
+            using(var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+            
+                if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    Console.WriteLine("Selected Path: " + fbd.SelectedPath);
+                    new BuildBat().Build(fbd.SelectedPath, BenchChoice.Cinebench);
+                    var cbResult = new CBRunner().Run();
+                    cbScore = cbResult.Split("(")[0];
+                    Console.WriteLine(cbResult);
+                }
+            }
+        }
+        public class CBRunner
+        {
+            public string Run()
+            {
+                // Process p = new Process();
+                // p.StartInfo.UseShellExecute = false;
+                // p.StartInfo.RedirectStandardOutput = true;
+                // p.StartInfo.FileName = @"bench.bat";
+                // p.Start();
+                // p.WaitForExit();
+
+                using (StreamReader rd = new StreamReader("text.txt"))
+                {
+                    string[] lines = rd.ReadToEnd()
+                        .Split(new string[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+                    return lines[lines.Length - 2];
+                }
+            }
+        }
     }
     
 }
